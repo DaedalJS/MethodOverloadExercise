be18614b33fa5dfaca4bb41d7fb8d54b33b65820
Josh Strawbridge <HolyKnightJoshua@gmail.com>
2020-10-29 20:10:40 -0700

    fix for non dollar cases

--- MethodOverloadExercise/Program.cs
+++ MethodOverloadExercise/Program.cs
@@ -8,17 +8,33 @@ namespace MethodOverloadExercise
 		{
 			return x + y;
 		}
+
+
 		static decimal Add(decimal x, decimal y)
 		{
 			return x + y;
 		}
+
+
 		static string Add(int x, int y, bool money)
 		{
-			return (money == true && Math.Abs(x + y) != 1) ? $"{x + y} dollars" : $"{x + y} dollar";
+			if (money == true)
+			{
+				return Math.Abs(x + y) != 1 ? $"{x + y} dollars" : $"{x + y} dollar";
+			}
+			else
+			{
+				return $"{x + y}";
+			}
+
 		}
+
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine(Program.Add(0, 1, true));
+			Console.WriteLine(Program.Add(-1, 1, true));
+			Console.WriteLine(Program.Add(2.1m, 1.4m));
+			Console.WriteLine(Program.Add(3, 4));
 		}
 	}
 }

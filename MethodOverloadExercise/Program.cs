edfbb8331e366f67dc3b6d9cb0c3cb51601560d7
Josh Strawbridge <HolyKnightJoshua@gmail.com>
2020-10-29 19:27:26 -0700

    Method Overloading Exercise

---
+++ MethodOverloadExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodOverloadExercise
+{
+    class Program
+    {
+		static int Add(int x, int y)
+		{
+			return x + y;
+		}
+		static decimal Add(decimal x, decimal y)
+		{
+			return x + y;
+		}
+		static string Add(int x, int y, bool money)
+		{
+			return (money == true && Math.Abs(x + y) != 1) ? $"{x + y} dollars" : $"{x + y} dollar";
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Program.Add(0, 1, true));
+		}
+	}
+}
